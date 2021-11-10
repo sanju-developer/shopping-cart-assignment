bc3c8337e4362a84e12dd7ba453cc9306efc6add
Vivek <vivek.rajoriya@publicissapient.com>
2021-11-10 17:03:28 +0530

    fixed footer

--- components/StyledComponents/Home/HomePageBanner.js
+++ components/StyledComponents/Home/HomePageBanner.js
@@ -2,8 +2,11 @@ import styled from "styled-components";
 
 export const HomePageBanner = styled.div`
   margin-top: 50px;
-  min-height: 250px;
   .carousel-root {
     box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px;
+  }
+
+  @media screen and (max-width: 480px) {
+    margin-top: 10px;
   }
 `;
